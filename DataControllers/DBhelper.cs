2ac6039a98df124153b0cd40dba5762551647344
qasimnauman <m.qasimnauman@gmail.com>
2024-06-02 23:56:14 +0500

    Added the DBhelper

---
+++ DataControllers/DBhelper.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataControllers
+{
+    public class DBhelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=QASIM-DESKTOP\\SQLEXPRESS;Initial Catalog=VPLabFinalQ1;Integrated Security=True");
+            return connection;
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Models
+{
+    public class UserModel
+    {
+        public int uid { get; set; }
+    }
+}
